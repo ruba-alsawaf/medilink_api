b51ea18b97d6b528f43703ee8d63985a57b01fde
ruba-alsawaf <rubaalsawaf2003@gmail.com>
2025-10-01 13:28:19 -0400

    add tables and models

--- database/migrations/2025_10_01_151815_create_partners_table.php
+++ database/migrations/2025_10_01_151815_create_partners_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('partners', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('contact_email');
+            $table->string('contact_email')->index();
             $table->timestamps();
         });
     }
